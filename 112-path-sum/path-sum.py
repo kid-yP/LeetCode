c16f400e08bbee999d716b01fe0d7a411402433d
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-04-21 22:33:46 +0300

    Time: 0 ms (100.00%) | Memory: 18.9 MB (43.08%) - LeetSync

---
+++ 112-path-sum/path-sum.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        queue = deque([(root, targetSum - root.val)])
+
+        while queue:
+            node, curr_sum = queue.popleft()
+
+            if not node.left and not node.right and curr_sum == 0:
+                return True
+
+            if node.left:
+                queue.append((node.left, curr_sum - node.left.val))
+            if node.right:
+                queue.append((node.right, curr_sum - node.right.val))
+
+        return False
