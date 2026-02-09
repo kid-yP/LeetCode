ef9a7522cb5bf03ea2c9cd44a36036c9a9e9da5a
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-02-09 11:13:54 +0300

    Time: 7 ms (30.95%) | Memory: 21.9 MB (7.09%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,8 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = 0
+        nat = Counter(nums)
 
+        kid = []
         for num in nums:
-            result ^= num
-
-        return result
+            if nat[num] == 1:
+                kid.append(num)
+        
+        return kid[0]
