0c11b621a87afa96208286b5b21e1efc4e9ff599
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-02-13 16:58:08 +0300

    Time: 2 ms (4.54%) | Memory: 19.4 MB (46.46%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,9 +1,10 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         words = s.split()
+        
         if len(pattern) != len(words):
             return False
-
+        
         char_to_word = {}
         word_to_char = {}
 
@@ -12,7 +13,7 @@ class Solution:
                 return False
             if word in word_to_char and word_to_char[word] != char:
                 return False
-
+            
             char_to_word[char] = word
             word_to_char[word] = char
 
