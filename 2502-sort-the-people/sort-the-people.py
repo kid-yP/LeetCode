f707476770b4aa5973c76ba3f5084fc6a545e028
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-02-18 17:16:14 +0300

    Time: 225 ms (17.14%) | Memory: 19.6 MB (73.50%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,21 +1,35 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        
         n = len(names)
-        
         for i in range(n):
-            swapped = False
-            
-            for j in range(0, n - i - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    names[j], names[j + 1] = names[j + 1], names[j]
+            max_idx = i
+            for j in range(i + 1, n):
+                if heights[max_idx] < heights[j]:
+                    max_idx = j
                     
-                    swapped = True
+            heights[i], heights[max_idx] = heights[max_idx], heights[i]
+            names[i], names[max_idx] = names[max_idx], names[i]
 
-            if not swapped:
-                break
-        
         return names
+
+
+        # n = len(names)
+        
+        # for i in range(n):
+        #     swapped = False
+            
+        #     for j in range(0, n - i - 1):
+        #         if heights[j] < heights[j + 1]:
+        #             heights[j], heights[j + 1] = heights[j + 1], heights[j]
+        #             names[j], names[j + 1] = names[j + 1], names[j]
+                    
+        #             swapped = True
+
+        #     if not swapped:
+        #         break
+        
+        # return names
         
         # n = len(names)
 
