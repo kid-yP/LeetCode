2639f874683ac134fd037a7c18e637f9895be3b2
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-02-20 16:05:45 +0300

    Time: 7 ms (66.94%) | Memory: 22.1 MB (54.25%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,12 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        cnt = Counter(nums)
+        # cnt = Counter(nums)
+        # n = len(nums)
+
+        # res = []
+        # for num in cnt:
+        #     if cnt[num] > n / 3:
+        #         res.append(num)
+
+        
+        # return res
+
+        #sorting
+        nums.sort()
         n = len(nums)
-
+        threshold = n // 3
         res = []
-        for num in cnt:
-            if cnt[num] > n / 3:
-                res.append(num)
-
+        
+        i = 0
+        while i < n:
+            j = i
+            while j < n and nums[j] == nums[i]:
+                j += 1
+            if j - i > threshold:
+                res.append(nums[i])
+            i = j
         
         return res
