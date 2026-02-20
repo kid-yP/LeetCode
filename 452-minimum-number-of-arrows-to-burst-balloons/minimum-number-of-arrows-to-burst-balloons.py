f5b691ea3cbf6848f859c56d37e9d57bfbad5e34
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-02-20 11:28:30 +0300

    Time: 82 ms (55.33%) | Memory: 53.5 MB (46.42%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -3,14 +3,13 @@ class Solution:
         if not points:
             return 0
 
-        points.sort(key=lambda x: x[1])
-
-        arrows = 1
-        end = points[0][1]
+        points.sort(key=lambda x :x[1])
+        count = 1
+        latest_end = points[0][1]
 
         for i in range(1, len(points)):
-            if points[i][0] > end:
-                arrows += 1
-                end = points[i][1]
-
-        return arrows
+            if points[i][0] > latest_end:
+                count += 1
+                latest_end = points[i][1]
+        
+        return count
