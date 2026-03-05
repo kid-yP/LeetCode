b5a5a3510f5436eed1190e73721f4a46ffe64bee
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-05 09:06:03 +0300

    Time: 67 ms (33.91%) | Memory: 42.5 MB (93.78%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.py
+++ 2465-shifting-letters-ii/shifting-letters-ii.py
@@ -1,25 +1,22 @@
-from typing import List
-
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         n = len(s)
-        ps = [0] * (n + 1)
-
+        p = [0] * (n + 1)
+        
         for start, end, direction in shifts:
             if direction == 1:
-                ps[start] += 1
-                ps[end + 1] -= 1
+                p[start] += 1
+                p[end + 1] -= 1
             else:
-                ps[start] -= 1
-                ps[end + 1] += 1
-        
+                p[start] -= 1
+                p[end + 1] += 1
+
         for i in range(1, n):
-            ps[i] += ps[i - 1]
-        
+            p[i] += p[i - 1]
+
         result = []
         for i in range(n):
             curr_pos = ord(s[i]) - ord('a')
-            new_pos = (curr_pos + ps[i]) % 26
+            new_pos = (curr_pos + p[i]) % 26
             result.append(chr(new_pos + ord('a')))
-        
         return "".join(result)
