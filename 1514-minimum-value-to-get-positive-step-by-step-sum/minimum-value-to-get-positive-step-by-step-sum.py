5031395130aea3fa9fc791f04ef83dc25097cf20
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-05 11:54:56 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (48.29%) - LeetSync

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+        prefix_sum = 0
+        min_sum = 0
+
+        for num in nums:
+            prefix_sum += num
+            min_sum = min(min_sum, prefix_sum)
+        return 1 - min_sum
