e23f5abd94c1e56d2b261eeec29b03429b4b5d78
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-07 00:20:11 +0300

    Time: 46 ms (95.97%) | Memory: 39.6 MB (44.12%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n = len(nums)
-        prefix_mod = {0: -1}
-        curr_sum = 0
-
+        freq = {0 : -1}
+        
+        p = 0
         if k == 0:
             for i in range(n - 1):
                 if nums[i] == 0 and nums[i + 1] == 0:
@@ -11,13 +11,13 @@ class Solution:
             return False
 
         for i in range(n):
-            curr_sum += nums[i]
-            remainder = curr_sum % k
+            p += nums[i]
+            remainder = p % k
 
-            if remainder in prefix_mod:
-                if i - prefix_mod[remainder] >= 2:
+            if remainder in freq:
+                if i - freq[remainder] >= 2:
                     return True
             else:
-                prefix_mod[remainder] = i
+                freq[remainder] = i
 
         return False
