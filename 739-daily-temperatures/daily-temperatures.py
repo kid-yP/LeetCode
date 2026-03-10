d2d65afac56cd7f491693d4374996ee7c07785f5
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-11 01:41:12 +0300

    Time: 145 ms (8.91%) | Memory: 39.5 MB (5.00%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,5 +1,6 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #Dictionary
         n = len(temperatures)
         stack = []
         mp = {}
@@ -14,3 +15,16 @@ class Solution:
             mp[i] = 0
 
         return [mp[i] for i in range(n)]
+        #Prefix sum
+
+        # stack = []
+        # n = len(temperatures)
+        # prefix_sum = [0] * n
+
+        # for i, temp in enumerate(temperatures):
+        #     while stack and temperatures[i] > temperatures[stack[-1]]:
+        #         prev = stack.pop()
+        #         prefix_sum[prev] = i - prev
+        #     stack.append(i)
+        
+        # return prefix_sum
