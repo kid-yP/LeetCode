e1e6639abe995d8e147653a13db8a9aeeaf2fd49
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-24 19:06:46 +0300

    Time: 0 ms (100.00%) | Memory: 44.1 MB (98.66%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -9,15 +9,14 @@ class Solution:
         if not root:
             return 0
         
-        queue = deque([(root, 1)])  # (node, depth)
-        
+        queue = deque([(root, 1)])
+
         while queue:
             node, depth = queue.popleft()
-            
-            # If we reach a leaf node, return the depth
+
             if not node.left and not node.right:
                 return depth
-            
+
             if node.left:
                 queue.append((node.left, depth + 1))
             if node.right:
