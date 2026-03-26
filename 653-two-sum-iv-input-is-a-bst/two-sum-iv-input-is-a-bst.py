7c042ce339b0ef6468f2b492a71f0fd20e622449
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-03-26 15:05:53 +0300

    Time: 6 ms (47.95%) | Memory: 22.4 MB (48.06%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        nums = []
+        
+        def inorder(node):
+            if not node:
+                return
+            inorder(node.left)
+            nums.append(node.val)
+            inorder(node.right)
+        
+        inorder(root)
+        
+        left, right = 0, len(nums) - 1
+        while left < right:
+            s = nums[left] + nums[right]
+            if s == k:
+                return True
+            elif s < k:
+                left += 1
+            else:
+                right -= 1
+        return False
