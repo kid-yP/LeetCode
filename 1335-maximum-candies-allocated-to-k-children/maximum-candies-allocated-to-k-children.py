2193992fdab900301903c2d060ba1fed39767cee
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-04-10 18:22:01 +0300

    Time: 424 ms (30.17%) | Memory: 30 MB (96.18%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        left, right = 1, max(candies)
+        ans = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            
+            children = sum(pile // mid for pile in candies)
+
+            if children >= k:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+    
+        return ans
