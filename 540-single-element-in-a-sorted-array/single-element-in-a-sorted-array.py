f4eebba132f5be563140ad97e6fc5b11572cd896
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2026-05-06 05:00:28 +0300

    Time: 9 ms (5.05%) | Memory: 29.1 MB (5.25%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNonDuplicate(self, nums: list[int]) -> int:
+        freq = {}
+        
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        
+        for num, count in freq.items():
+            if count == 1:
+                return num
